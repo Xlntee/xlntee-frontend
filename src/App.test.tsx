dbe14978e409315a4c5bcc5d1ce53217ebf064b7
Eugene Kozoriz <king59510@gmail.com>
2024-02-14 12:07:12 +0200

    initial-test-setup vitest+react-testing-library

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import App from "./App";
+import { render, screen, userEvent } from "./utils/test-utils";
+
+describe("App component test", () => {
+
+    it('render App.tsx', () => {
+        render(<App/>)
+    })
+
+});
