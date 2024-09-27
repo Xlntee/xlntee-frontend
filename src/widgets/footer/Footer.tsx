306e78237dba05e5cd844b75f6f1499a9817a86e
Jekoz <zkozoriz11@gmail.com>
2024-09-27 13:34:17 +0300

    Resolved requested changes

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -1,66 +1,78 @@
-import { Box, Container, Stack, Typography, Link as MuiLink } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { Box, Container, Stack, Typography, Link as MuiLink, Grid } from "@mui/material";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
-
-import FooterLinkButton from "./ui/FooterLinkButton";
 
 import { XlnteeColors } from "src/shared/themes/colors";
 
 import "./Footer.scss";
 
 const Footer = () => {
+  //TODO: add links
+  //TODO: localization
+
   return (
     <Box component="footer">
-      <Container sx={{ borderTop: `1px solid ${XlnteeColors.GrayStrokeColor}` }} className="footer">
-        <Box className="footer__container-main-text">
-          <Typography variant="body1" className="footer__main-text">
-            Ми працюємо над тим, щоб зробити користування платформою якомога зручнішим. Ви можете допомогти нам стати
-            кращими
-          </Typography>
-        </Box>
-        <Stack flexDirection="row" alignItems="center" justifyContent="center" gap="20px" flexWrap="wrap">
-          <FooterLinkButton path="#">Центр Підтримки</FooterLinkButton>
-          <FooterLinkButton path="#">Умови використання</FooterLinkButton>
-          <FooterLinkButton path="#">Політика конфіденційності</FooterLinkButton>
-          <Stack flexDirection="row" gap="10px" alignItems="center">
-            <Typography variant="body2" sx={{ fontSize: "14px" }}>
-              Зв'яжіться з нами:
-            </Typography>
-            <FooterLinkButton path="mailto:support@xlntee.com">support@xlntee.com</FooterLinkButton>
-          </Stack>
-        </Stack>
-
-        <Stack
-          flexDirection={{
-            xs: "column",
-            md: "row",
-          }}
-          gap={{ lg: "10px" }}
-          className="footer__social-network-block"
-        >
-          <MuiLink sx={{ color: XlnteeColors.BlackElementColor }} href="https://www.instagram.com/" target="_blank">
-            <InstagramIcon className="footer__social-network-icon" sx={{ fontSize: "20px" }} />
-          </MuiLink>
-          <MuiLink sx={{ color: XlnteeColors.BlackElementColor }} href="https://www.linkedin.com/" target="_blank">
-            <LinkedInIcon className="footer__social-network-icon" sx={{ fontSize: "20px" }} />
-          </MuiLink>
-          <MuiLink sx={{ color: XlnteeColors.BlackElementColor }} href="https://www.facebook.com/" target="_blank">
-            <FacebookIcon className="footer__social-network-icon" sx={{ fontSize: "20px" }} />
-          </MuiLink>
-        </Stack>
-
-        <Stack
-          flexDirection="row"
-          className="footer__cards-images-block"
-          display={{
-            xs: "none",
-            lg: "flex",
-          }}
-        >
-          <img className="footer__cards-image" src="/assets/mastercard-logo.png" />
-          <img className="footer__cards-image" src="/assets/visa-logo.png" />
-        </Stack>
+      <Container className="footer">
+        <Grid container spacing={2}>
+          <Grid item xs={12} lg={2}>
+            <Stack
+              gap="10px"
+              display="flex"
+              flexDirection="row"
+              alignItems={{ xs: "center", lg: "flex-start" }}
+              justifyContent={{ xs: "center", lg: "flex-start" }}
+            >
+              <MuiLink color={XlnteeColors.BlackElementColor} href="https://www.instagram.com/" target="_blank">
+                <InstagramIcon className="footer__social-network-icon" sx={{ fontSize: "20px" }} />
+              </MuiLink>
+              <MuiLink color={XlnteeColors.BlackElementColor} href="https://www.linkedin.com/" target="_blank">
+                <LinkedInIcon className="footer__social-network-icon" sx={{ fontSize: "20px" }} />
+              </MuiLink>
+              <MuiLink color={XlnteeColors.BlackElementColor} href="https://www.facebook.com/" target="_blank">
+                <FacebookIcon className="footer__social-network-icon" sx={{ fontSize: "20px" }} />
+              </MuiLink>
+            </Stack>
+          </Grid>
+          <Grid item xs={12} lg={8}>
+            <Box className="footer__container-main-text">
+              <Typography variant="caption" className="footer__main-text">
+                Ми працюємо над тим, щоб зробити користування платформою якомога зручнішим. Ви можете допомогти нам
+                стати кращими
+              </Typography>
+            </Box>
+            <Stack flexDirection="row" alignItems="center" justifyContent="center" gap="20px" flexWrap="wrap">
+              <Link className="footer__link" to="#">
+                Центр Підтримки
+              </Link>
+              <Link className="footer__link" to="#">
+                Умови використання
+              </Link>
+              <Link className="footer__link" to="#">
+                Політика конфіденційності
+              </Link>
+              <Typography variant="caption">
+                Зв'яжіться з нами:{" "}
+                <MuiLink sx={{ textDecoration: "none" }} href="mailto:support@xlntee.com">
+                  support@xlntee.com
+                </MuiLink>
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            lg={2}
+            display="flex"
+            alignItems={{ xs: "center", lg: "flex-end" }}
+            justifyContent={{ xs: "center", lg: "flex-end" }}
+          >
+            <img className="footer__cards-image" src="/assets/mastercard-logo.png" />
+            <img className="footer__cards-image" src="/assets/visa-logo.png" />
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   );
