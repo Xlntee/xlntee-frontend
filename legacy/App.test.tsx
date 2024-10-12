9d575fd2d75206bda851beef77484d7306f51afe
vasilkovalyov <vasilkovalyov@gmail.com>
2024-10-12 14:14:01 +0200

    removed legacy folder

--- legacy/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import App from "../src/app/App";
-import { render } from "./test/test-utils";
-
-describe("App component test", () => {
-  it("render App.tsx", () => {
-    render(<App />);
-  });
-});
