a80b3ad90dacef5933b0bdcfff425945a5df5fa0
vasilkovalyov <vasilkovalyov@gmail.com>
2024-11-03 12:11:58 +0100

    refactoring info-course-complain component

--- src/widgets/components/course-complain/ui/info-course-complain/InfoCourseComplain.tsx
+++ src/widgets/components/course-complain/ui/info-course-complain/InfoCourseComplain.tsx
@@ -5,12 +5,10 @@ import { Stack, Box, Typography } from "@mui/material";
 interface InfoCourseComplainProps {
   image: string;
   imageDescription: string;
-  message1: string;
-  message2: string;
-  code: string;
+  message: string;
 }
 
-const InfoCourseComplain: FC<InfoCourseComplainProps> = ({ image, imageDescription, message1, code, message2 }) => {
+const InfoCourseComplain: FC<InfoCourseComplainProps> = ({ image, imageDescription, message }) => {
   return (
     <Stack
       direction={{ sm: "row" }}
@@ -22,10 +20,7 @@ const InfoCourseComplain: FC<InfoCourseComplainProps> = ({ image, imageDescripti
         <img src={image} alt={imageDescription} width="100%" />
       </Box>
       <Box maxWidth={{ md: "380px" }} width="100%">
-        <Typography variant="body1" mb="20px">
-          {message1} <strong>{code}</strong>
-        </Typography>
-        <Typography variant="body1">{message2}</Typography>
+        <Typography variant="body1" mb="20px" dangerouslySetInnerHTML={{ __html: message }} />
       </Box>
     </Stack>
   );
