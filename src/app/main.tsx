b15fcb56379fda5723a46db309d71ea164881b33
vasilkovalyov <vasilkovalyov@gmail.com>
2024-11-28 10:20:01 +0100

    installed google-analitics

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,13 @@ import "normalize.css";
 import "react-quill/dist/quill.snow.css";
 import "src/styles/main.scss";
 import ReactDOM from "react-dom/client";
+import ReactGa from "react-ga4";
 import App from "./App.tsx";
+
+ReactGa.initialize("G-ZB1X5QVSP7");
+ReactGa.send({
+  hitType: "pageview",
+  page: window.location.pathname
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
